462b40a6b5543e565629c61dc107b3fe7cbbe1db
martynovappolina <polinagalaxytab27@gmail.com>
2021-03-13 01:11:28 +0300

    complete

--- lab3/2.js
+++ lab3/2.js
@@ -8,7 +8,18 @@
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
 function getMinMax(str) {
-    // code here
+    let new_str = '';
+    let space = ' ';
+    for (let i in str) {
+        if (parseInt(el[index])) {
+            new_str = new_str + el[index]
+            if (str[i + 1] == ' ' || str[i + 1] == '')
+                new_str += space;
+        }
+    }
+    new_str.split(' ').map(parseInt);
+    return 'min: ' + Math.min(new_str) + ', max: ' + Math.max(new_str)
+
 }
 
 module.exports = getMinMax;
