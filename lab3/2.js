6c3d62436137b2348129ac7cf67325ec4c65e53e
martynovappolina <polinagalaxytab27@gmail.com>
2021-03-13 01:43:27 +0300

    ccc

--- lab3/2.js
+++ lab3/2.js
@@ -8,16 +8,18 @@
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
 function getMinMax(str) {
-    let new_str = '';
-    let space = ' ';
-    for (let i in str) {
-        if (parseInt(str[i])) {
-            new_str = new_str + str[i]
-            if (!parseInt(str[i + 1]))
-                new_str += space;
-        }
-    }
-    new_str.split(' ').map(parseInt);
+    // let new_str = '';
+    // let space = ' ';
+    // for (let i in str) {
+    //     if (parseInt(str[i])) {
+    //         new_str = new_str + str[i]
+    //         if (!parseInt(str[i + 1]))
+    //             new_str += space;
+    //     }
+    // }
+
+    // new_str.split(' ').map(parseInt);
+    str.split(/\D+(?!$)/).map(parseFloat);
     return 'min: ' + Math.min(new_str) + ', max: ' + Math.max(new_str)
 
 }
