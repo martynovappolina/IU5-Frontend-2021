6a018f4f3db4b935890e750b3fb16742993f8c13
martynovappolina <polinagalaxytab27@gmail.com>
2021-03-13 02:57:12 +0300

    complete

--- lab3/2.js
+++ lab3/2.js
@@ -19,7 +19,7 @@ function getMinMax(str) {
     // }
     // new_str.split(' ').map(parseInt);
 
-    str.split(/(-?\d+(\.\d+)?)/g).map(parseFloat);
+    str.match(/(\d+)/).map(parseFloat);
     return 'min: ' + Math.min(str) + ', max: ' + Math.max(str)
 
 }
