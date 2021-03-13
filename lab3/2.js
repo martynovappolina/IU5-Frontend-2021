eeaf5e7f164945ccab8e822baf520198f9ae3e1d
martynovappolina <polinagalaxytab27@gmail.com>
2021-03-13 03:17:24 +0300

    fhgj

--- lab3/2.js
+++ lab3/2.js
@@ -19,7 +19,7 @@ function getMinMax(str) {
     // }
     // new_str.split(' ').map(parseFloat);
 
-    str.match(/(-?\d+(\.\d+)?)/g).map(n => +n);
+    str.match(/(-?\d+(\.\d+)?+\d)/).map(parseFloat);
     return 'min: ' + Math.min(str) + ', max: ' + Math.max(str)
 
 }
