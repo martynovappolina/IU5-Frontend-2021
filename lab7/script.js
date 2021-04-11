24bbd8a4c97ada7c1caead2c38ff2f1e172ccfbe
martynovappolina <polinagalaxytab27@gmail.com>
2021-04-11 21:01:50 +0300

    complete l7

---
+++ lab7/script.js
@@ -0,0 +1,30 @@
+function main() {
+
+    let ApiKey = "8d18bd354dfdf3483d47c4eb0497434b";
+    let city = city_search.value === "" ? "Москва" : encodeURIComponent(city_search.value);
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&units=metric&appid=" + ApiKey;
+    axios.get(url).then(res => {
+        document.querySelector('.City').innerHTML = "Погода в городе " + res.data.name
+        document.querySelector('.temperature').innerHTML = (Math.round(res.data.main.temp) >= 0 ? "+" + Math.round(res.data.main.temp) : Math.round(res.data.main.temp)) + "°"
+        document.querySelector('.condition').innerHTML = res.data.weather[0].description[0].toUpperCase() + res.data.weather[0].description.slice(1)
+        document.querySelector('.humidity').innerHTML = Math.round(res.data.main.humidity) + "%"
+        document.querySelector('.wind').innerHTML = res.data.wind.speed + "м/с"
+        document.querySelector('.pressure').innerHTML = Math.round(res.data.main.pressure * 0.75) + "мм рт. ст."
+
+        document.querySelector('.dateh1').innerHTML = res.dt;
+        document.querySelector('.temperatureh1').innerHTML = (Math.round(res.data.main.temp) >= 0 ? "+" + Math.round(res.data.main.temp) : Math.round(res.data.main.temp)) + "°"
+        document.querySelector('.conditionh1').innerHTML = res.data.weather[0].description[0].toUpperCase() + res.data.weather[0].description.slice(1)
+
+
+        document.querySelector('.temperatureh2').innerHTML = (Math.round(res.data.main.temp) >= 0 ? "+" + Math.round(res.data.main.temp) : Math.round(res.data.main.temp)) + "°"
+        document.querySelector('.conditionh2').innerHTML = res.data.weather[0].description[0].toUpperCase() + res.data.weather[0].description.slice(1)
+
+        document.querySelector('.temperatureh3').innerHTML = (Math.round(res.data.main.temp) >= 0 ? "+" + Math.round(res.data.main.temp) : Math.round(res.data.main.temp)) + "°"
+        document.querySelector('.conditionh3').innerHTML = res.data.weather[0].description[0].toUpperCase() + res.data.weather[0].description.slice(1)
+
+
+    })
+
+
+
+}
